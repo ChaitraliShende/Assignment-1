0ed7476fe12722c116fa75a01d2120da64707d3f
ChaitraliShende <chaitralishende2502@gmail.com>
2025-10-08 20:47:44 +0530

    Add files via upload

---
+++ Assignment 1(Task 1).py
@@ -0,0 +1,7 @@
+#Task 1
+A=float(input("Enter the first number:"))
+B=float(input("Enter the second number:"))
+print('Addition:',A+B)
+print('Substraction:',A-B)
+print('Multiplication:',A*B)
+print('Division:',A/B)
---
+++ Assignment 1(Task 2).py
@@ -0,0 +1,4 @@
+#Task 2
+C=input("Enter the first name:")
+D=input("Enter the last name:")
+print('Hello,',C,D,',Welcome to this interacting programme.')
