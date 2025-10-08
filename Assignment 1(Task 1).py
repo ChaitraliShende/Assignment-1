21ebf6c38b7dcc80d0fb08acf950cff6e1862f1e
ChaitraliShende <chaitralishende2502@gmail.com>
2025-10-08 20:46:53 +0530

    Delete Assignment 1(Task 1).py

--- Assignment 1(Task 1).py
+++
@@ -1,9 +0,0 @@
-#Task 1
-A=12
-B=4
-print('Enter the first number:',A)
-print('Enter the second number:',B)
-print('Addition:',A+B)
-print('Substraction:',A-B)
-print('Multiplication:',A*B)
-print('Division:',A/B)
